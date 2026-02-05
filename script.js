67f7f263bb7c70feccc715bd483200c329e77233
zainaskari137-creator <zainaskari137@gmail.com>
2026-02-05 23:54:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function sayHello() {
-  alert("Hello! Your website is working!");
-}
+// simple scroll animation
+const cards = document.querySelectorAll('.card, .faq-box');
+
+window.addEventListener('scroll', () => {
+  const trigger = window.innerHeight * 0.85;
+
+  cards.forEach(card => {
+    const top = card.getBoundingClientRect().top;
+    if (top < trigger) {
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
